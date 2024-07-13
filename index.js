cd2917d0f6795baee2539665be9af651b9bb3b97
WahibFer <wahibx@gmail.com>
2024-07-13 13:10:46 +0200

    Add new route

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.get('/new-route', (req, res) => {
+    res.send('This is a new route!');
+  });
+  
